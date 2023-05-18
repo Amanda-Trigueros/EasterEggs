79b3fc9225dc92c8d33d1a49b845b7328f825310
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-05-18 14:22:28 -0500

    Acode Loader function

--- easter-eggs.js
+++ easter-eggs.js
@@ -2,6 +2,13 @@ function TicTacToe() {
 }
 
 function Loader() {
+    this.loaderCharacters = ['|', '/', '-', "\\"]
+    function load() {
+        for (let i=0; i < this.loaderCharacters.length; i++) {
+            console.clear();
+            return console.log(this.loaderCharacters[i]);
+        }
+    }
 }
 
 function Clock() {
